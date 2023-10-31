9bc7cc8329a5d51115c9a2e543a76daafba03fb7
DeepakKumarSingh27 <69988805+DeepakKumarSingh27@users.noreply.github.com>
2023-10-31 11:13:14 +0530

    First Commit

---
+++ IfStmt1.sh
@@ -0,0 +1,6 @@
+read -p " Enter first Number : " a
+read -p " Enter second Number : " b
+if [ $a -gt $b ]
+then 
+echo $a is greater then $b
+fi
---
+++ addition.sh
@@ -0,0 +1,7 @@
+read -p " Enter ist Number: " x
+read -p " Enter 2nd Number: " y
+echo $x 
+echo $y
+z=$(( $x + $y ))
+echo $z
+
